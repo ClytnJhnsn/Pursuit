7a591930516d97f7cac83ced4b07174be6b0534f
joh17757 <joh17757@umn.edu>
2021-12-13 09:52:52 -0600

    shouldn't need to do this

--- src/main.cc
+++ src/main.cc
@@ -2,20 +2,32 @@
 #include <iostream>
 
 int main() {
-    Sim* sim = new Sim();
-    std::cout << sim->GetDist() << std::endl;
+    Sim* sim1 = new Sim();
+    std::cout << "****" << std::endl;
+    std::cout << "Sim1" << std::endl;
+    std::cout << "****" << std::endl;
+    std::cout << sim1->GetDist() << std::endl;
     for (int i = 0; i < 100; i++) {
-        sim->Update();
-        std::cout << sim->GetDist() << std::endl;
-        if (sim->Caught(2.0)) { 
+        sim1->Update();
+        std::cout << sim1->GetDist() << std::endl;
+        if (sim1->Caught(2.0)) { 
             i = 100; 
-            std::cout << "********" << std::endl;
             std::cout << "Caught!" << std::endl;
-            std::cout << "********" << std::endl;
         }
-        // if (i==50) {
-        //     std::vector<int> poop;
-        //     int stopper = poop[10];
-        // }
     }
+
+    Sim* sim2 = new Sim();
+    std::cout << "****" << std::endl;
+    std::cout << "Sim2" << std::endl;
+    std::cout << "****" << std::endl;
+    std::cout << sim2->GetDist() << std::endl;
+    for (int i = 0; i < 100; i++) {
+        sim2->Update();
+        std::cout << sim2->GetDist() << std::endl;
+        if (sim2->Caught(2.0)) { 
+            i = 100; 
+            std::cout << "Caught!" << std::endl;
+        }
+    }
+
 }
